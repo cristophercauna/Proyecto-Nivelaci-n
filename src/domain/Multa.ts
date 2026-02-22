dd8a3804e0ed2d86c63a587708754bf1bf8950d0
cristophercauna <ccaunam@unjbg.edu.pe>
2026-02-22 15:36:18 -0500

    Se agrega el estado de la multa para bloquear prestamos.

--- src/domain/Multa.ts
+++ src/domain/Multa.ts
@@ -1,9 +1,14 @@
 import { Usuario } from "./Usuario";
-
+export enum EstadoMulta {
+    PENDIENTE,
+    PAGADA
+}
 export class Multa{
     private fecha: Date;
+    private estado: EstadoMulta;
     constructor(private usuario: Usuario,private monto: number,private motivo: string){
         this.fecha = new Date();
+        this.estado= EstadoMulta.PENDIENTE;
     }
     public getUsuario(): Usuario{
         return this.usuario;
