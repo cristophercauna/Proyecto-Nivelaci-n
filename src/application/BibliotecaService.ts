78bb725d2970750da923d80b3e4afd70e1562c3d
cristophercauna <ccaunam@unjbg.edu.pe>
2026-02-22 15:45:01 -0500

    Se añade las funcones obetenerPrestamos activos ( para ver que prestamos tiene el usuario) y tieneMultasPendientes para ver las multas pendientes del usuario.

--- src/application/BibliotecaService.ts
+++ src/application/BibliotecaService.ts
@@ -2,7 +2,7 @@ import { Usuario } from "../domain/Usuario";
 import { RecursoBiblioteca,EstadoRecurso } from "../domain/RecursoBiblioteca";
 import { Prestamo } from "../domain/Prestamo";
 import { Reserva } from "../domain/Reserva";
-import { Multa } from "../domain/Multa";
+import { Multa,EstadoMulta } from "../domain/Multa";
 export class BibliotecaService {
     private usuarios: Usuario[] = [];
     private recursos: RecursoBiblioteca[] = [];
@@ -16,17 +16,18 @@ export class BibliotecaService {
     public registrarRecurso(recurso: RecursoBiblioteca): void {
         this.recursos.push(recurso);
     }
-    private obtenerPrestamosActivos(usuario: Usuario): Prestamo[] {
-        return this.prestamos.filter(p =>
-            p.getUsuario().getId() === usuario.getId()
-        );
+    private obtenerPrestamosActivos(usuario: Usuario): Prestamo[]{
+        return this.prestamos.filter(p =>p.getUsuario().getId() === usuario.getId() && !p.estaDevuelto);
     }
-    public prestarRecurso(usuario: Usuario, recurso: RecursoBiblioteca): void {
+     private tieneMultasPendientes(usuario: Usuario): boolean{
+        return this.multas.some(m =>m.getUsuario().getId() === usuario.getId() &&m.estaPendiente());
+    }
+    public prestarRecurso(usuario: Usuario, recurso: RecursoBiblioteca): void{
         if (recurso.getEstado() !== EstadoRecurso.DISPONIBLE) {
             throw new Error("El recurso no está disponible");
         }
         const prestamosActivos = this.obtenerPrestamosActivos(usuario);
-        if (prestamosActivos.length >= usuario.obtenerLimitePrestamos()) {
+        if (prestamosActivos.length >= usuario.obtenerLimitePrestamos()){
             throw new Error("Límite de préstamos alcanzado");
         }
         const prestamo = new Prestamo(usuario, recurso, new Date());
