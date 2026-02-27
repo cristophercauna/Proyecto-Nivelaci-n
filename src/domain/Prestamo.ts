defa32bacc58ba1e3fa4050014855565c5ccc949
cristophercauna <ccaunam@unjbg.edu.pe>
2026-02-26 22:24:41 -0500

    Se agregan las validaciones si el recurso esta disponible y si el prestamo ya esta devuelto.

--- src/domain/Prestamo.ts
+++ src/domain/Prestamo.ts
@@ -4,11 +4,17 @@ export class Prestamo{
     private fechaDevolucion?: Date;
     private fechaLimite : Date;
     constructor(private usuario:Usuario,private recurso: RecursoBiblioteca, private fechaPrestamo: Date){
+        if(!recurso.estaDisponible()){
+            throw new Error("Es recurso no esta disponible");
+        }
         this.fechaLimite = new Date(fechaPrestamo);
         this.fechaLimite.setDate(this.fechaLimite.getDate() + this.recurso.obtenerDuracionPrestamo());
         this.recurso.cambiarEstado(EstadoRecurso.PRESTADO);
     }
     public marcarDevolucion(){
+        if(this.fechaDevolucion){
+            throw new Error("El prestamo ya fue devuelto");
+        }
         this.fechaDevolucion = new Date();
         this.recurso.cambiarEstado(EstadoRecurso.DISPONIBLE); 
     }
